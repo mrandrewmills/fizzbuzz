f8322e855d28de6d4c4e24b4d0903b6a6b3ba9d9
Andrew Mills <andrewmills@knowwwhow.com>
2015-03-22 15:18:03 -0400

    initial commit

---
+++ tests.js
@@ -0,0 +1,33 @@
+QUnit.test( "default values initialization", function(assert) {
+  assert.equal(fizzbuzz.start, 1, "We expect start to be 1.");
+  assert.equal(fizzbuzz.finish, 100, "We expect start to be 100.");
+  assert.equal(fizzbuzz.modFizz, 3, "We expect modFizz to be 3.");
+  assert.equal(fizzbuzz.modBuzz, 5, "We expect modBuzz to be 5.");
+  assert.equal(fizzbuzz.txtFizz, "fizz", "We expect txtFizz to be 'fizz.'");
+  assert.equal(fizzbuzz.txtBuzz, "buzz", "We expect txtBuzz to be 'buzz.'");
+});
+
+results = fizzbuzz.play();
+
+QUnit.test( "check quantity of results", function(assert) {
+  assert.equal(results.length, 101, "We expect play method to return 101 elements [JavaScript uses 0-based arrays].");
+});
+
+QUnit.test( "check individual results", function(assert){
+  for (x = fizzbuzz.start; x <= fizzbuzz.finish; x++) {
+    assert.ok(
+      (results[x] == x) ||
+      (results[x] == 'fizz') ||
+      (results[x] == 'buzz') ||
+      (results[x] == 'fizzbuzz') , "We expect results[x] is equal to x, 'fizz', 'buzz', or 'fizzbuzz.'");
+  }
+});
+
+QUnit.test( "verify default values were not altered", function(assert) {
+  assert.equal(fizzbuzz.start, 1, "We expect start to be 1.");
+  assert.equal(fizzbuzz.finish, 100, "We expect start to be 100.");
+  assert.equal(fizzbuzz.modFizz, 3, "We expect modFizz to be 3.");
+  assert.equal(fizzbuzz.modBuzz, 5, "We expect modBuzz to be 5.");
+  assert.equal(fizzbuzz.txtFizz, "fizz", "We expect txtFizz to be 'fizz.'");
+  assert.equal(fizzbuzz.txtBuzz, "buzz", "We expect txtBuzz to be 'buzz.'");
+});
