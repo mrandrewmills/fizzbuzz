875cebbb937f2e994fd50fcce5ea44e3d7acfe50
Andrew Mills <andrewmills@knowwwhow.com>
2016-11-16 22:59:29 -0500

    fixing typo

--- gruntfile.js
+++ gruntfile.js
@@ -2,4 +2,4 @@ grunt.loadNpmTasks('grunt-contrib-qunit');
     gruntConfig.qunit = {
         src: ['src/test.html']
     };
-grunt.registerTask('test', 'qunit:src');
+grunt.registerTask('travis', 'qunit:src');
